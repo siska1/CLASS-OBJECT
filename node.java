83e6e43df47c9dc48c1bb41cdf34f8dde6ad7ef9
siska1 <44000389+siska1@users.noreply.github.com>
2018-10-10 00:50:16 +0800

    Create node.java

---
+++ node.java
@@ -0,0 +1,33 @@
+
+public class Node {
+
+	String Label;
+	int Value;
+	
+	//ini method getter
+	public String getLabel()
+	{
+		return Label;
+	}
+	
+	public int getValue()
+	{
+		return Value;
+	}
+	
+	//ini method setter
+	public void setLabel(String Label)
+	{
+		this.Label = Label;
+	}
+	public void setValue(int Value)
+	{
+		this.Value = Value;
+	}
+
+
+
+
+	}
+
+
